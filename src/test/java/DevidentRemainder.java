3d198185b569a49274949656848c89bb9562bfa4
Thisal Kodithuwakku <33801630+Harryjer@users.noreply.github.com>
2025-08-04 15:35:13 +0530

    Finding devident and remainder of given values

---
+++ src/test/java/DevidentRemainder.java
@@ -0,0 +1,12 @@
+public class DevidentRemainder {
+    public static void main(String[]args){
+        int devidend = 30;
+        int devisior = 5;
+
+        int quation = devidend / devisior;
+        int remainder = devidend % devisior;
+
+        System.out.println("quation is " + quation);
+        System.out.println("remainder is " + remainder);
+    }
+}
